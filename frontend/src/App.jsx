415d31deba1631227b103304cb92d5d14c8c74da
pratap26pps <pankajpatna10321@gmail.com>
2025-03-24 21:47:59 +0530

    vite and tailwind setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <>
+      
+      <h1 class="text-3xl font-bold bg-green-400 flex justify-center  underline">
+         Welcome to Ten.Ai
+      </h1> 
+  
+    </>
+  )
+}
+
+export default App

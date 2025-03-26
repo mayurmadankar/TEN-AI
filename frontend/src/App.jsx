1135b97b1c1eac4cda9f8f1caf2be6489e50b05c
Ashu Negi <anegi6723@gmail.com>
2025-03-26 12:53:07 +0530

    Add routing and layout components; update jsconfig and package dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,17 @@
-import './App.css'
-import { Compare } from './components/ui/Compare'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip"
-function App() {
-   
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
 
-  return (
-    <>
-      
-      <h1 class="text-3xl font-bold bg-green-400 flex justify-center  underline">
-         Welcome to Ten.Ai
-      </h1> 
-       
-      <hi>shad-cn setup successfully</hi>
-      <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-        
-          <p className='border bg-cyan-400 w-16 cursor-pointer'> Hover</p>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Add to library</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  
-      <div className="p-4 flex flex-col justify-center items-center rounded-3xl ">
-      <h1 class=" font-bold   flex justify-center">
-         examples for using animated component from ui.aceternity
-      </h1> 
-      <Compare
-        firstImage="https://assets.aceternity.com/code-problem.png"
-        secondImage="https://assets.aceternity.com/code-solution.png"
-        firstImageClassName="object-cover object-left-top"
-        secondImageClassname="object-cover object-left-top"
-        className="h-[250px] w-[200px] md:h-[500px] md:w-[500px]"
-        slideMode="hover"
-      />
-    </div>
-    </>
-  )
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+        <Route element={<Layout />} >
+				  <Route path="/" element={<Home />} />
+        </Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;

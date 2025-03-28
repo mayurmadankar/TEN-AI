4db317b127aeea7f994484db4eec32b4fefb6c08
pratap26pps <pankajpatna10321@gmail.com>
2025-03-28 13:55:56 +0530

    header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-
 function App() {
 	return (
 		<BrowserRouter>
+		 
 			<Routes>
         <Route element={<Layout />} >
 				  <Route path="/" element={<Home />} />

3f504cbdfbac1351fd89fae8b619ad98b6ec37c8
Ashu Negi <anegi6723@gmail.com>
2025-03-29 13:22:46 +0530

    Add Demo component to Home page and adjust layout styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
+import Demo from '@/components/Demo'
 import Hero from '@/components/Hero'
 import React from 'react'
 
 function Home() {
   return (
-    <div>
+    <div className='flex flex-col items-center'>
         <Hero />
+        <Demo />
     </div>
   )
 }

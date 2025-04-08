5cc09d31a3f20cf591a8d30a9c8fe2c8a4ec4365
Uma Shankar Bharawa <ubharawa@gmail.com>
2025-04-08 10:08:06 +0530

    Refactor Experts component;

--- frontend/src/components/Experts.jsx
+++ frontend/src/components/Experts.jsx
@@ -6,8 +6,8 @@ export default function Experts() {
     <section className="pt-20 pb-20">
         <div className="w-full pr-10 pl-10">
             <div className="z-1 text-center flex flex-col flex-nowrap items-center relative">
-                <h2 className="text-4xl sm:text-5xl md:text-6xl text-black mt-0 mb-0 font-serif font-semibold leading-[140%] ">Led by Experts and <br /> Backed by Top Investors</h2>
-                <div className="w-[50%] max-md:w-[80%] text-center mb-8 text-sm sm:text-base text-[#0009] font-serif leading-[150%]">At TEN AI, our team is composed of engineers from the world’s leading tech companies and top educational institutions. These experts drive the innovative capabilities of our agents, ensuring we deliver cutting-edge AI solutions.</div>
+                <h2 className="text-4xl sm:text-5xl md:text-6xl mt-0 mb-0 font-serif font-semibold leading-[140%] ">Led by Experts and <br /> Backed by Top Investors</h2>
+                <div className="w-[50%] max-md:w-[80%] text-center mb-8 text-sm sm:text-base font-serif leading-[150%]">At TEN AI, our team is composed of engineers from the world’s leading tech companies and top educational institutions. These experts drive the innovative capabilities of our agents, ensuring we deliver cutting-edge AI solutions.</div>
                 <div className="flex gap-x-8 gap-y-8 justify-center mt-4 mr-auto mb-6 ml-auto">
                     <img src="https://cdn.prod.website-files.com/66796a37e9aadbf3722c4340/66913098d0aa065b20c64512_General%20Catalyst%20Logo.svg" className="h-[2rem] align-middle max-w-full max-md:w-[25%] inline-block" />
                     <div className="border-l-2"></div>
